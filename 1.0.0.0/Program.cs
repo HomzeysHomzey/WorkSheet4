f722253c326cdaabfcb409a44eb7bdacfbd02c90
HomzeysHomzey <158465267+HomzeysHomzey@users.noreply.github.com>
2024-02-13 16:47:08 +0000

    Add project files.

---
+++ 1.0.0.0/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Week4Q1Part1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsvalidEmployeeID("yes"));
+        }
+        static bool IsvalidEmployeeID(string input)
+        {
+            Console.Write("Enter Password : ");
+            input = Console.ReadLine();
+            if (input == "")
+            {
+                Console.WriteLine("must be at least 6 characters...");
+                return false;
+            }
+            return true;
+
+        }
+    }
+}
